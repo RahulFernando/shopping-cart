aacb54019e824302eb7aa0dfd803b66db8f496d8
RahulFernando <rahulrulz680@gmail.com>
2022-02-25 10:29:33 +0530

    feat: register & card ui

--- src/containers/home/container.tsx
+++ src/containers/home/container.tsx
@@ -1,10 +1,57 @@
 import React from 'react';
+import { Row, Col } from 'antd';
+
+import Card from '../../components/card';
+
+// style
+import './style.css';
+
+const data = [
+  {
+    cover:
+      'https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png',
+  },
+  {
+    cover:
+      'https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png',
+  },
+  {
+    cover:
+      'https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png',
+  },
+  {
+    cover:
+      'https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png',
+  },
+  {
+    cover:
+      'https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png',
+  },
+  {
+    cover:
+      'https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png',
+  },
+  {
+    cover:
+      'https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png',
+  },
+  {
+    cover:
+      'https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png',
+  },
+];
 
 const Container: React.FC = () => {
   return (
-    <div>
-      <h2>Home</h2>
-    </div>
+    <Row style={{ rowGap: '10px' }}>
+      {data.map((item, index) => (
+        <Col className="items-card">
+          <Card key={index} cover={item.cover}>
+            <h1>Hello</h1>
+          </Card>
+        </Col>
+      ))}
+    </Row>
   );
 };
 
